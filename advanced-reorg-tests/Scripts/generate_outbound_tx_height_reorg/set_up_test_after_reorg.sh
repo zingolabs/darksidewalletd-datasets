a1d818237c5c705736dfe57d45b74c8c2e585006
Francisco Gindre <francisco.gindre@gmail.com>
2023-10-23 16:20:42 -0300

    Fix dataset for before reorg and after reorg script

--- advanced-reorg-tests/Scripts/generate_outbound_tx_height_reorg/set_up_test_after_reorg.sh
+++ advanced-reorg-tests/Scripts/generate_outbound_tx_height_reorg/set_up_test_after_reorg.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 
-grpcurl -plaintext -d '{ "url" : "http://localhost:8000/changes-inbound-tx-mined-height/after_reorg.txt" }' localhost:9067 cash.z.wallet.sdk.rpc.DarksideStreamer/StageBlocks
+grpcurl -plaintext -d '{ "url" : "http://localhost:8000/changes-outbound-tx-mined-height/after_reorg.txt" }' localhost:9067 cash.z.wallet.sdk.rpc.DarksideStreamer/StageBlocks
 
 grpcurl -plaintext -d '{ "height" : 215 }' localhost:9067 cash.z.wallet.sdk.rpc.DarksideStreamer/ApplyStaged
